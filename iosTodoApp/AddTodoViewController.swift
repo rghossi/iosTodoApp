c6e67824db2975c92e49812157f332ee6399918a
rghossi <rafael.ghossi@gmail.com>
2017-04-04 18:30:02 -0300

    added navigation controllers and changed view controller class name

---
+++ iosTodoApp/AddTodoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosTodoApp
+//
+//  Created by Rafael Rodrigues Ghossi on 4/4/17.
+//  Copyright © 2017 Rafael Rodrigues Ghossi. All rights reserved.
+//
+
+import UIKit
+
+class AddTodoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
